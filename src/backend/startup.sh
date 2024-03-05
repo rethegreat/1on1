7b3b0ac83bc54ea107f2f0b5d0305f605b2c22f8
Irene Kang <irene.kang@mail.utoronto.ca>
2024-03-05 17:29:43 -0500

    Added run.sh and startup.sh as described in the handout

---
+++ src/backend/startup.sh
@@ -0,0 +1,17 @@
+# 1. Go to the directory you want
+cd OneOnOne
+
+# 2. Create a virtual environment
+python -m venv venv
+
+# 3. Activate the virtual environment
+source venv/bin/activate
+
+# 4. Install Django
+python -m pip install Django
+
+# 5. Install all required packages
+pip install django pillow djangorestframework djangorestframework-simplejwt
+
+# 6. Run all migrations
+python manage.py migrate
