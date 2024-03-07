42ead1dfa48e1740a71b175a53fcdf75869c6d37
Kurtis Law <kurtislaww@gmail.com>
2024-03-07 15:15:35 -0500

    changed AUTH_USER_MODEL, added to INSTALLED_APPS

--- src/backend/django_src/django_src/settings.py
+++ src/backend/django_src/django_src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_src.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
