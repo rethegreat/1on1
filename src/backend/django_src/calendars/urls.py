a3205886096e4d1f7fe84db7993b12bbba994908
Irene Kang <irene.kang@mail.utoronto.ca>
2024-03-10 14:42:41 -0400

    added remindAll url in urls.py

--- src/backend/django_src/calendars/urls.py
+++ src/backend/django_src/calendars/urls.py
@@ -5,6 +5,7 @@ from .views import calendars, members, owner_availability, member_availability, 
 urlpatterns = [
     path('list/', calendars.CalendarList.as_view(), name='calendar-list'),
     path('<int:calendar_id>/', calendars.CalendarDetail.as_view(), name='calendar-detail'),
+    path('<int:calendar_id>/remindAll/', calendars.CalendarRemind.as_view(), name='member-remind'),
 
     path('<int:calendar_id>/members/list/', members.MemberListView.as_view(), name='member-list'),
     path('<int:calendar_id>/members/<int:member_id>/', members.MemberDetailView.as_view(), name='member-detail'),
