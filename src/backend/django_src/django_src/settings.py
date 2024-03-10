2a0a202b5ff0c0538140f79fb7eede5414c1961b
Wo Ming Boaz Cheung <92407344+rethegreat@users.noreply.github.com>
2024-03-10 14:42:44 -0400

    added gitignore

--- src/backend/django_src/django_src/settings.py
+++ src/backend/django_src/django_src/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'emails',
     'calendars',
     'accounts',
     'rest_framework.authtoken',
