95ada2eb8678736c4dc6d1fc03bad84d06bd1d68
Irene Kang <irene.kang@mail.utoronto.ca>
2024-03-11 02:01:59 -0400

    Added an auto finalize feature in signals.py + apps.py(not yet tested)

--- src/backend/django_src/calendars/apps.py
+++ src/backend/django_src/calendars/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CalendarsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'calendars'
+
+    def ready(self):
+        # Import signal handlers and connect them here
+        from . import signals
