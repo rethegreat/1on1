33e9889a1bdf1b9b935df9119e1f2aa9d68b99c1
Wo Ming Boaz Cheung <92407344+rethegreat@users.noreply.github.com>
2024-05-31 14:41:58 -0400

    use aws enviroment variable

--- src/backend/django_src/django_src/settings.py
+++ src/backend/django_src/django_src/settings.py
@@ -4,15 +4,6 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-
-env_file = BASE_DIR / '.env'  
-
-if env_file.is_file():
-    env.read_env(str(env_file))
-else:
-    print("failed")
-    
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-jg0_afyqqlc*!fjs*cxq6!*%1!7wf)xy4wlafn3_8#o_s*4#%m'
 
@@ -28,8 +19,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER') 
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Application definition
 
